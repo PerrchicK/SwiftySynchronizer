138251dc27c4e9a777f6b5a76d14a0443fb5d967
Perry Sh <pshalev@liveperson.com>
2018-05-06 12:45:54 +0300

    Adding project files + podspec file

---
+++ Pods/Target Support Files/Pods-SwiftySynchronizer/Pods-SwiftySynchronizer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftySynchronizer : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftySynchronizer
+@end
---
+++ SwiftySynchronizer/SwiftySynchronizer.h
@@ -0,0 +1,19 @@
+//
+//  SwiftySynchronizer.h
+//  SwiftySynchronizer
+//
+//  Created by Perry Shalev on 06/05/2018.
+//  Copyright © 2018 Perry Sh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftySynchronizer.
+FOUNDATION_EXPORT double SwiftySynchronizerVersionNumber;
+
+//! Project version string for SwiftySynchronizer.
+FOUNDATION_EXPORT const unsigned char SwiftySynchronizerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftySynchronizer/PublicHeader.h>
+
+
